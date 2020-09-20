836909792573582b0344ca9ad1a2609fe9e55bc1
caiyuan <caiyuan@live.com>
2020-09-20 17:35:34 +0800

    Install Kind

--- install-kind.sh
+++ install-kind.sh
@@ -6,7 +6,7 @@
 
 ## install kind:latest
 KIND_VERSION="v0.9.0"
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-$(uname)-amd64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-$(uname)-amd64 --progress-bar
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 kind version
