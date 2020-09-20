d3712d9610b9160f9486f263747977da64d8264a
caiyuan <caiyuan@live.com>
2020-09-20 17:28:10 +0800

    Install Kind

---
+++ install-kind.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#
+# https://github.com/kubernetes-sigs/kind/releases/latest
+#
+
+## install kind:latest
+KIND_VERSION="v0.9.0"
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-$(uname)-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+kind version
+
+## create a cluster
+kind create cluster
+
